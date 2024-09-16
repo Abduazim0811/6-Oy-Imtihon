4498216ca5ed46894977d9d961e63c0c9bbcd2d2
Your Name <dostonxoshimov2005@gmail.com>
2024-09-17 04:36:06 +0500

    swagger host is modified

--- api-gateway/internal/app/docs/docs.go
+++ api-gateway/internal/app/docs/docs.go
@@ -1331,7 +1331,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:9006",
+	Host:             "3.127.221.197:9006",
 	BasePath:         "/",
 	Schemes:          []string{"https"},
 	Title:            "Artisan Connect",
